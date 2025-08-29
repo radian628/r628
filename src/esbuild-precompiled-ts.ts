265538817fe06b316a36065363a1684ed803d125
radian628 <70352880+radian628@users.noreply.github.com>
2025-08-28 18:07:36 -0700

    Create esbuild-precompiled-ts.ts

---
+++ src/esbuild-precompiled-ts.ts
@@ -0,0 +1,37 @@
+import * as esbuild from "esbuild";
+import * as path from "node:path";
+
+export const bundledPrecompiledTypescript = (
+  buildSettings: Parameters<typeof esbuild.build>[0]
+): esbuild.Plugin => ({
+  name: "bpt",
+  setup(build) {
+    build.onResolve({ filter: /\?.*bpt/ }, (args) => {
+      return {
+        path: path.join(args.resolveDir, args.path),
+        namespace: "bpt-ns",
+      };
+    });
+    build.onLoad({ filter: /.*/, namespace: "bpt-ns" }, async (args) => {
+      const fspath = args.path.replace(/\?.*$/, "");
+
+      const result = await esbuild.build({
+        entryPoints: [fspath],
+        bundle: true,
+        outfile: "index.js",
+        write: false,
+        plugins: [],
+      });
+
+      for (const file of result.outputFiles) {
+        if (file.path.endsWith("index.js")) {
+          return {
+            contents: new TextDecoder("utf8").decode(file.contents),
+            loader: "text",
+            watchFiles: [fspath],
+          };
+        }
+      }
+    });
+  },
+});
