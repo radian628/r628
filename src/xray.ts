19ac08936a5563e768871439db0b6ceedf29e91c
radian628 <70352880+radian628@users.noreply.github.com>
2025-11-08 12:58:01 -0800

    Update xray.ts

--- src/xray.ts
+++ src/xray.ts
@@ -31,47 +31,82 @@ type NestedModification<Obj, Chain, Value> = Chain extends [
 
 type NestedModKey = string | number | symbol;
 
-type XRay<Root, Access, Context, Chain extends NestedModKey[]> =
+type XRayWithModification<
+  InitRoot,
+  Root,
+  NewAccess,
+  Context,
+  Chain extends NestedModKey[],
+> = XRay<
+  InitRoot,
+  NestedModification<Root, Chain, NewAccess>,
+  NewAccess,
+  Context,
+  Chain
+>;
+
+type XRay<InitRoot, Root, Access, Context, Chain extends NestedModKey[]> =
   // xray methods
   {
     $v: Root;
-    $: <T>(a: T) => XRay<NestedModification<Root, Chain, T>, T, Context, Chain>;
+    $: <T>(
+      a: T
+    ) => XRay<InitRoot, NestedModification<Root, Chain, T>, T, Context, Chain>;
     $s: <T>(
       fn: (a: Access, c: Context) => T
-    ) => XRay<NestedModification<Root, Chain, T>, T, Context, Chain>;
-    $ctx: <T>(fn: (a: Access, s: Context) => T) => XRay<Root, Access, T, Chain>;
+    ) => XRay<InitRoot, NestedModification<Root, Chain, T>, T, Context, Chain>;
+    $ctx: <T>(
+      fn: (a: Access, s: Context) => T
+    ) => XRay<InitRoot, Root, Access, T, Chain>;
   } &
     // array-specific xray methods
     (Access extends (infer Elem)[]
       ? {
           $ec: <T>(
             fn: (i: number, c: Context) => number
-          ) => XRay<Root, Elem, T, [...Chain, number]>;
-          $e: XRay<Root, Elem, number, [...Chain, number]>;
+          ) => XRay<InitRoot, Root, Elem, T, [...Chain, number]>;
+          $e: XRay<InitRoot, Root, Elem, number, [...Chain, number]>;
         }
       : {}) &
     // object-specific xray methods
     (Access extends Record<infer K, infer V>
       ? {
-          [K in keyof Access]: XRay<Root, Access[K], Context, [...Chain, K]>;
+          [K in keyof Access]: XRay<
+            InitRoot,
+            Root,
+            Access[K],
+            Context,
+            [...Chain, K]
+          >;
         } & {
-          $m: <
-            T extends {
-              [K in keyof Access]?: (
-                e: XRay<Root, Access[K], Context, [...Chain, K]>
-              ) => XRay<any, any, any, any[]>;
-            },
-          >(
-            a: T
-          ) => XRay<
-            Omit<Root, keyof T> & {
-              [K in keyof T]: T[K] extends (
-                ...p: any[]
-              ) => XRay<infer R2, infer A2, any, any[]>
-                ? A2
+          $m: <T extends Record<any, any>>(
+            cb: (a: Access) => T
+          ) => XRayWithModification<
+            InitRoot,
+            Root,
+            Omit<Access, keyof T> & T,
+            Context,
+            Chain
+          >;
+
+          $mx: <T extends Record<any, XRay<any, any, any, any, any>>>(
+            cb: (t: {
+              [K in keyof Access]: XRay<
+                Access[K],
+                Access[K],
+                Access[K],
+                Context,
+                [...Chain, K]
+              >;
+            }) => T
+          ) => XRayWithModification<
+            InitRoot,
+            Root,
+            Omit<Access, keyof T> & {
+              [K in keyof T]: T[K] extends XRay<any, infer Root, any, any, any>
+                ? Root
                 : never;
             },
-            Access,
             Context,
             Chain
           >;
@@ -89,18 +124,33 @@ const dummy = {
 let asd = undefined as unknown as XRay<
   typeof dummy,
   typeof dummy,
+  typeof dummy,
   undefined,
   []
 >;
+
+export const xray = <A>(a: A): XRay<A, A, A, undefined, []> =>
+  undefined as unknown as XRay<A, A, A, undefined, []>;
 
 // export const xr: <R, A, C, Ch extends NestedModKey[]>() => ((
 //   x: XRay<R, A, C, Ch>
 // ) => XRay<R, A, C, Ch>) &
 //   XRay<R, A, C, Ch> = undefined;
 
-const test2 = asd.c.$([4, 5]).$v;
+const id: <T>(t: T) => T = (x) => x;
 
-const test3 = asd.$m({
-  fuck: (e) => e.$(69),
-  c: (e) => e.$(420),
-}).$v;
+const test2 = asd.c.$([4, 5]);
+
+const test3 = asd.$m((x) => ({
+  fuck: ["penis"],
+  cunt: 3,
+})).$v.cunt;
+
+const test4 = asd.$mx((o) => ({
+  fuck: o.fuck.$(["penis"]),
+  cunt: xray(3),
+})).$v.fuck;
+
+// const test5 = asd.$f //
+//   .fuck((f) => f.$(["penis"])) //
+//   .$nf.cunt(() => x(3)).$v; //
